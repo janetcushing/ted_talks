bf915d15fcfa177bb0178026b47828510410ea07
janetcushing <janet.cushing@gmail.com>
2018-02-11 14:52:44 -0500

    it sorta works, still has some bugs in the modal, but it deletes and adds notes to the database

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -1,3 +1,19 @@
+
+function displayNotesModal(thisId, modal){
+    console.log("im in displayNotesModal");
+        // var button = $(event.relatedTarget) // Button that triggered the mod
+        console.log(`thisId ${thisId}`);
+        console.log("this id is " + thisId);
+        $.ajax({
+            type: "GET",
+            url: "/api/prevnotes/" + thisId
+        }).then(
+            function (notes) {
+                console.log(notes)
+                modal.find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
+            });
+        // modal.find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
+}
 //=================================================
 // On Page Load
 //=================================================
@@ -7,10 +23,11 @@ $(document).ready(function () {
 
     // function getTheNotes(thisId) {
     //     var modal = $(this);
+    //     thisId = button.data('id');
     //     // ajax to get the previous notes goes here
     //     $.ajax({
     //         type: "GET",
-    //         url: "/api/prevnotes2/5a7e021e3607104b31139d05"
+    //         url: "/api/prevnotes/" + thisId
     //     }).then(
     //         function (notes) {
     //             console.log(notes)
@@ -18,68 +35,80 @@ $(document).ready(function () {
     //         });
     // }
 
-    
+
     $('#exampleModal').on('show.bs.modal', function (event) {
+        var modal = $(this);
         console.log("i clicked the button to show the modal");
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('whatever') // Extract info from data-* attributes
-        // thisId = button.data('id');
-        thisId = "5a7e021e3607104b31139d05";
+        // var recipient = button.data('whatever') // Extract info from data-* attributes
+        thisId = button.data('id');
         console.log("this id is " + thisId);
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
+       
 
         // ajax to get the previous notes goes here
-        $.ajax({
-            type: "GET",
-            url: "/api/prevnotes2/5a7e021e3607104b31139d05"
-        }).then(
-            function (notes) {
-                console.log(notes)
-                modal.find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
-            });
+        // $.ajax({
+        //     type: "GET",
+        //     url: "/api/prevnotes/" + thisId
+        // }).then(
+        //     function (notes) {
+        //         console.log(notes)
+        modal.find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
+        // });
     });
 
-    // $('#exampleModal').on('click', '#save-note', function (event) {
-    //     console.log("i clicked the save-note button");
-    //     var note = $('#message-text').val().trim();
-    //     if (note) {
-    //         var newNote = {
-    //             "id": thisId,
-    //             "note": note
-    //         };
-    //         console.log("newNote");
-    //         console.log(newNote);
-    //         $.ajax({
-    //             type: "PUT",
-    //             url: "/api/note/" + thisId,
-    //             data: newNote
-    //         });
-    //     } else {
-    //         $('#message-text').attr("placeholder", "* You must enter a note or click the close button");
-    //     }
-    // });
+    $('#exampleModal').on('click', '#save-note', function (event) {
+        console.log("i clicked the save-note button");
+        var modal = $(this);
+        var note = $('#message-text').val().trim();
+        if (note) {
+            var newNote = {
+                "id": thisId,
+                "note": note
+            };
+            console.log("newNote");
+            console.log(newNote);
+            $.ajax({
+                type: "PUT",
+                url: "/api/note/" + thisId,
+                data: newNote
+            }).then(
+                function (notes) {
+                    console.log(notes);
+                    displayNotesModal(thisId, modal);
+                    // $(this).find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
+                });
+        } else {
+            $('#message-text').attr("placeholder", "* You must enter a note or click the close button");
+        }
+    });
 
-    // $('#exampleModal').on('click', '#delete-note', function (event) {
-    //     console.log("i clicked the delete-note button");
-    //     // Make sure to preventDefault on a submit event. 
-    //     event.preventDefault();
-    //     var noteId = $(this).attr("data-id");
-    //     var note = $(this).attr("data-whatever");
-    //     console.log(`this id is ${thisId}`);
-    //     var articleId = '5a7e021e3607104b31139d05';
-    //     deleteObj = {'articleId': articleId, 'note': note};
-    //     //  Send the POST request.
-    //     $.ajax({
-    //         type: "PUT",
-    //         url: "/api/deletenote/" + articleId,
-    //         data: deleteObj
-    //     }).then(function (res) {
-    //         console.log("i'm back from the server side");
-    //         console.log(res);
-    //         location.reload();
-    //     });
-    // });
+    $('#exampleModal').on('click', '#delete-note', function (event) {
+        console.log("i clicked the delete-note button");
+        // Make sure to preventDefault on a submit event. 
+        event.preventDefault();
+        var modal = $(this);
+        // var noteId = $(this).attr("data-id");
+        var note = $(this).attr("data-whatever");
+        console.log("this note is " + note);
+        console.log(`this id is ${thisId}`);
+        // var articleId = '5a7e021e3607104b31139d05';
+        deleteObj = {
+            'articleId': thisId,
+            'note': note
+        };
+        console.log(deleteObj);
+        //  Send the POST request.
+        $.ajax({
+            type: "PUT",
+            url: "/api/deletenote/" + thisId,
+            data: deleteObj
+        }).then(function (res) {
+            console.log("i'm back from the server side");
+            console.log(res);
+            displayNotesModal(thisId, modal);
+            // $(this).find('.modal-title').text(`Notes for Ted Talk  ${thisId}`);
+            // location.reload();
+        });
+    });
 
 });
